0377c182ba1dcdb45790ac5d3e8871f5b5b6bd1d
Vandankav <kvandy0709@gmail.com>
2025-04-23 12:26:02 +0530

    query fix

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -8,7 +8,6 @@ import { motion } from "framer-motion";
 const Jobs = () => {
   const { allJobs, searchedQuery } = useSelector((store) => store.job);
   const [filterJobs, setFilterJobs] = useState(allJobs);
-
   useEffect(() => {
     const { location, title, salary } = searchedQuery || {};
     const filteredJobs = allJobs.filter((job) => {
